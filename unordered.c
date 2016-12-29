8bf42cb5a27d4fa165cabf1eb6ef56655e1829a9
ab303 <abhishek303.4@gmail.com>
2016-12-29 12:04:00 -0500

    Update unordered.c

--- unordered.c
+++ unordered.c
@@ -21,198 +21,163 @@ struct Message {
         int id;
 };
 
-int msg_id;
+int msg_id; //randomly generated identifier for each node
 
 int x=0;// number of nodes
 
 void *sender(void *a) 
 {
 	struct sockaddr_in addr;
-    struct Message msg;
-    int fd, cnt;
-    struct ip_mreq mreq;
-    //char *message="destroy";
-    
-    //printf("enter sender\n");
-    
-    //u_char loopch;
-    time_t t;
+	struct Message msg;
+	int fd, cnt;
+	struct ip_mreq mreq;
+	//char *message="destroy";
+	//printf("enter sender\n");
+	//u_char loopch;
+	time_t t;
+	
+	srand((unsigned) time(&t));
+	msg_id=rand()%50;
+	printf("my id is %d\n\n", msg_id );
+	sleep(3); // time to start all nodes
+	
+	/*create socket*/
+    	if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0)
+     	{
+      		perror("socket");
+      		exit(1);
+     	}
 
-    srand((unsigned) time(&t));
 
+    	/* set up destination address */
+    	memset(&addr,0,sizeof(addr));
+     	addr.sin_family=AF_INET;
+     	addr.sin_addr.s_addr=inet_addr(HELLO_GROUP);
+     	addr.sin_port=htons(HELLO_PORT);
 
-
-    msg_id=rand()%50;
-    printf("my id is %d\n\n", msg_id );
-    sleep(3); // time to start all nodes
-
-
-
-    
-    /*create socket*/
-    if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0)
-     {
-      perror("socket");
-      exit(1);
-     }
-
-     // setsocket here
-     /*
-    if (setsockopt(fd,IPPROTO_IP,IP_MULTICAST_LOOP,&loopch,sizeof(loopch)) < 0) {
-       perror("disabling multicast loopback failed");
-       exit(1);
-       }*/
-
-    /* set up destination address */
-     memset(&addr,0,sizeof(addr));
-     addr.sin_family=AF_INET;
-     addr.sin_addr.s_addr=inet_addr(HELLO_GROUP);
-     addr.sin_port=htons(HELLO_PORT);
-
-     msg.id=msg_id;
+     	msg.id=msg_id;
      
-     strcpy(msg.message,"random message:");
+     	strcpy(msg.message,"random message:");
      
-     /* now just sendto() our destination! */
-     while (1) {
-      if (sendto(fd,&msg,sizeof(struct Message),0,(struct sockaddr *) &addr,
-             sizeof(addr)) < 0) {
-           perror("sendto");
-            
-           exit(1);
-      }
-       //i++;
-      // printf("this is value of i %d \n", i);
-     sleep(2);
-     }
-//sleep(2);
+     	/* now just sendto() our destination! */
+     	while (1) 
+	{
+      		if (sendto(fd,&msg,sizeof(struct Message),0,(struct sockaddr *) &addr,
+             	sizeof(addr)) < 0)
+		{
+           		perror("sendto");
+        		exit(1);
+      		}
+       	sleep(2);
+	}
+
 
 }
 
 void *rcver(void *a)
  {
 
+
 	struct sockaddr_in addr;
-    struct Message my_msg;
-    {
-        
-    };
-    int fd, nbytes,addrlen;
-    struct ip_mreq mreq;
-    //char msgbuf[MSGBUFSIZE];
-    u_int yes=1;
-    //u_char loopch;
-    int n=0, all_ids[100],j=0,flag=0;
+    	struct Message my_msg;
+	int fd, nbytes,addrlen;
+    	struct ip_mreq mreq;
+    	//char msgbuf[MSGBUFSIZE];
+    	u_int yes=1;
+    	//u_char loopch;
+    	int n=0, all_ids[100],j=0,flag=0;
 
 
 
 
-    if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0) {
-      perror("socket");
-      exit(1);
-     }
+    	if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0)
+	{
+      		perror("socket");
+      		exit(1);
+     	}
 
-    /* allow multiple sockets to use the same ADD and PORT number */
-    if (setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(yes)) < 0) {
-       perror("Reusing ADDR failed");
-       exit(1);
-       }
-    if (setsockopt(fd,SOL_SOCKET,SO_REUSEPORT,&yes,sizeof(yes)) < 0) {
-       perror("Reusing ADDR failed");
-       exit(1);
-       }
-    //if (setsockopt(fd,IPPROTO_IP,IP_MULTICAST_LOOP,&loopch,sizeof(loopch)) < 0) {
-       //perror("disabling multicast loopback failed");
-     //  exit(1);
-     //  }
-
-
-
-
-
-     /* set up destination address */
-     memset(&addr,0,sizeof(addr));
-     addr.sin_family=AF_INET;
-     addr.sin_addr.s_addr=htonl(INADDR_ANY); /* N.B.: differs from sender */
-     addr.sin_port=htons(HELLO_PORT);
+    	/* allow multiple sockets to use the same ADD and PORT number */
+    	if (setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(yes)) < 0)
+	{
+       		perror("Reusing ADDR failed");
+       		exit(1);
+       	}
+    	if (setsockopt(fd,SOL_SOCKET,SO_REUSEPORT,&yes,sizeof(yes)) < 0) 
+	{
+       		perror("Reusing ADDR failed");
+       		exit(1);
+       	}
+	
+    	/*if (setsockopt(fd,IPPROTO_IP,IP_MULTICAST_LOOP,&loopch,sizeof(loopch)) < 0) 
+	{
+       		perror("disabling multicast loopback failed");
+     		exit(1);
+     	 }*/
+	//set this opt only when you test on different machines.
+	
+     	/* set up destination address */
+     	memset(&addr,0,sizeof(addr));
+     	addr.sin_family=AF_INET;
+     	addr.sin_addr.s_addr=htonl(INADDR_ANY); /* N.B.: differs from sender */
+    	addr.sin_port=htons(HELLO_PORT);
      
-     /* bind to receive address */
-     if (bind(fd,(struct sockaddr *) &addr,sizeof(addr)) < 0) {
-      perror("bind");
-      exit(1);
-     }
+     	/* bind to receive address */
+     	if (bind(fd,(struct sockaddr *) &addr,sizeof(addr)) < 0) 
+	{
+      		perror("bind");
+      		exit(1);
+     	}
      
-     /* use setsockopt() to request that the kernel join a multicast group */
-     mreq.imr_multiaddr.s_addr=inet_addr(HELLO_GROUP);
-     mreq.imr_interface.s_addr=htons(INADDR_ANY);
-     if (setsockopt(fd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) {
-      perror("setsockopt");
-      exit(1);
-     }
+     	/* use setsockopt() to request that the kernel join a multicast group */
+     	mreq.imr_multiaddr.s_addr=inet_addr(HELLO_GROUP);
+     	mreq.imr_interface.s_addr=htons(INADDR_ANY);
+     	if (setsockopt(fd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) 
+	{
+      		perror("setsockopt");
+      		exit(1);
+     	}
 
-     all_ids[n]=msg_id;
-     n++;
-     /* now just enter a read-print loop */
-      while(1)
-    {
-      //printf("entered while at Receiver\n");
-      addrlen=sizeof(addr);
-      if ((nbytes=recvfrom(fd,&my_msg,sizeof(struct Message),0,(struct sockaddr *) &addr,&addrlen)) < 0) 
-      {
-           perror("recvfrom");
-           exit(1);
-      }
-      //i++;
-      //printf("%d\n", my_msg.id );
-      //printf("%d\n", msg_id );
-
-
-
-      for(j=0;j<n;j++)
-      {
-        if(my_msg.id==all_ids[j])
-        {
-            flag=0;
-            break;
-        }
-    }
-    if(j==n)
-    {
-        flag=1;
-    }
+     	all_ids[n]=msg_id;
+     	n++;
+     	/* now just enter a read-print loop */
+      	while(1)
+    	{
+      		addrlen=sizeof(addr);
+      		if ((nbytes=recvfrom(fd,&my_msg,sizeof(struct Message),0,(struct sockaddr *) &addr,&addrlen)) < 0) 
+      		{
+           		perror("recvfrom");
+           		exit(1);
+      		}
+		//LINEAR SEARCH TO CHECK IF THE MESSAGE HAS ALREADY BEEN RECIEVED
+      		for(j=0;j<n;j++)
+      		{
+        		if(my_msg.id==all_ids[j])
+        		{
+            			flag=0;
+            			break;
+        		}
+    		}
+    		if(j==n)
+    		{
+        		flag=1;
+    		}
 
 
-        if(flag==1)
-        {
-            //printf("%d the value all_ids[j]\n",all_ids[j] );
-            all_ids[n]=my_msg.id;
-            //printf("%d the value all_ids[n]\n",all_ids[n] );
-            n++;
-           // printf("%d the value of n\n", n);
-            printf("%s %d\n", my_msg.message, my_msg.id  );
-          //  printf("%d value of x\n", x);
-            if(n==x)
-            {
-               // flag=0;
-               // printf("entered the flag setting\n");
-                goto EndTransmission;
-            }
+        	if(flag==1)
+        	{
+            		all_ids[n]=my_msg.id;
+            		n++;
+            		printf("%s %d\n", my_msg.message, my_msg.id  );
+            		if(n==x)
+            		{
+               			goto EndTransmission;
+            		}
 
-        }
+        	}
 
-
-      
-      //printf("exit for\n");
-
-
-      
-      //printf("this is the i value %d\n", i);
-      sleep(1);
-     }
-    // out of while
-     EndTransmission:printf("ending of recieving messages\n");
-
-
+      		sleep(1);
+     	}
+     	EndTransmission:printf("ending of recieving messages\n");
 }
 
 
@@ -245,6 +210,5 @@ int main(int argc, char *argv[])
      pthread_join(thread_r, NULL);
      
 
-    // sleep(50);
      return 0;
 }
